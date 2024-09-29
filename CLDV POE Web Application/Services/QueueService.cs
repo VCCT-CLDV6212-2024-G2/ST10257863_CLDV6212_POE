81ffa211a049748f393595e8b8021e6df10ff5b9
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-09-29 10:56:32 +0200

    Fixing misstype of student number. Added Azure Functions template with functionality.

--- CLDV POE Web Application/Services/QueueService.cs
+++ CLDV POE Web Application/Services/QueueService.cs
@@ -4,16 +4,16 @@ namespace CLDV_POE_Web_Application.Services
 {
 	public class QueueService
 	{
-		private readonly QueueServiceClient st10257864queueservice;
+		private readonly QueueServiceClient st10257863queueservice;
 
 		public QueueService(IConfiguration configuration)
 		{
-			st10257864queueservice = new QueueServiceClient(configuration["AzureStorage:ConnectionString"]);
+			st10257863queueservice = new QueueServiceClient(configuration["AzureStorage:ConnectionString"]);
 		}
 
 		public async Task SendMessageAsync(string queueName, string message)
 		{
-			var queueClient = st10257864queueservice.GetQueueClient(queueName);
+			var queueClient = st10257863queueservice.GetQueueClient(queueName);
 			await queueClient.CreateIfNotExistsAsync();
 			await queueClient.SendMessageAsync(message);
 		}
--- CLDV POE Web Application/Services/TableService.cs
+++ CLDV POE Web Application/Services/TableService.cs
@@ -7,19 +7,19 @@ namespace CLDV_POE_Web_Application.Services
 {
 	public class TableService
 	{
-		private readonly TableClient st10257864TableService;
+		private readonly TableClient st10257863TableService;
 
 		public TableService(IConfiguration configuration)
 		{
 			var connectionString = configuration["AzureStorage:ConnectionString"];
 			var serviceClient = new TableServiceClient(connectionString);
-			st10257864TableService = serviceClient.GetTableClient("st10257864tablesservice");
-			st10257864TableService.CreateIfNotExists();
+			st10257863TableService = serviceClient.GetTableClient("st10257863tablesservice");
+			st10257863TableService.CreateIfNotExists();
 		}
 
 		public async Task AddEntityAsync(CustomerProfile profile)
 		{
-			await st10257864TableService.AddEntityAsync(profile);
+			await st10257863TableService.AddEntityAsync(profile);
 		}
 	}
 }
